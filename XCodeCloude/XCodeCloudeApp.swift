f65a4f659405ed939838db65b61a0e6a7f5f3c25
Darshan <darshanpanchal619@gmail.com>
2022-04-22 18:07:11 +0530

    Initial Commit

---
+++ XCodeCloude/XCodeCloudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCodeCloudeApp.swift
+//  XCodeCloude
+//
+//  Created by darshan on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct XCodeCloudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
